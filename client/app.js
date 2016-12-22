f82e3ddeb7f0b2cfd07630d176170aa8e5fde32b
Angie Washburn <angiewashburn1997@gmail.com>
2016-12-22 12:49:07 -0500

    Initial Commit

--- client/app.js
+++ client/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+
+	var test = $.ajax({
+			type: "GET",
+			url: "http://localhost:3000/api/test"
+		});
+		test.done(function(data){
+			console.log(data);
+		});
+
+		test.fail(function(){
+			console.log("Oh no!");
+		});
+
 	$("#testAPI").on("click", function(){
 		console.log("It's working");
 	});
--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var app = express();
+
+app.use('/api/test', function(req, res){
+	res.send('Hello World');
+});
 
 app.listen(3000, function(){
-	console.log("app is listening on 3000");
+console.log('app is listening on 3000');
 });
